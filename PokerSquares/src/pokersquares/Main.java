426f7cad1041ff51d27a164a14665b8aa253135d
KaroAntonio <karoantonio@gmail.com>
2015-01-11 21:56:27 -0700

    Main Update

--- PokerSquares/src/pokersquares/Main.java
+++ PokerSquares/src/pokersquares/Main.java
@@ -6,6 +6,7 @@ import pokersquares.players.BeemoV2;
 
 public class Main {
     public static void main(String[] args){
+        //Start Here
         new PokerSquares(new BeemoV2(), GAME_MILLIS).playSequence(10000, 0, false);
     }
 }
