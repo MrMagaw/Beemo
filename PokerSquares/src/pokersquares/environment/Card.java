00d08fe66472ca0e1460593c23e6e91ab40dbcea
KaroAntonio <karoantonio@gmail.com>
2015-01-19 10:06:36 -0700

    Parametrized Environment
    test

--- PokerSquares/src/pokersquares/environment/Card.java
+++ PokerSquares/src/pokersquares/environment/Card.java
@@ -1,41 +1,100 @@
-package pokersquares.environment;
-
 import java.util.Arrays;
 import java.util.HashMap;
 
 /**
- * Card - Simple playing card class.
+ * Card - Playing card class representing all 52 cards of a French deck.
  * @author Todd W. Neller
  */
 public class Card {
-        
-	public static final int NUM_RANKS = 13, NUM_SUITS = 4;
-	public static Card[] allCards;
-	public static String[] rankNames = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"};
-	public static String[] suitNames = {"C", "D", "H", "S"};
-	public static HashMap<String, Card> cardMap = new HashMap<String, Card>();
+
+	// Static definitions
+	
+	/**
+	 * the number of ranks in the 52-card French deck
+	 */
+	public static final int NUM_RANKS = 13;
+	/**
+	 * the number of suits in the 52-card French deck
+	 */
+	public static final int NUM_SUITS = 4;
+	/**
+	 * the number of cards in the 52-card French deck
+	 */
+	public static final int NUM_CARDS = NUM_RANKS * NUM_SUITS;
+
+	private static Card[] allCards; // an array of all cards in the deck
+	private static String[] rankNames = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"}; // all single-character rank names
+	private static String[] suitNames = {"C", "D", "H", "S"}; // all single-character suit names
+	private static HashMap<String, Card> cardMap = new HashMap<String, Card>(); // mapping from String representations of cards to Card objects
 
 	static {
+		// upon loading the Card class, initialize 52-card French deck cards
+		
 		// create all card objects
 		allCards = new Card[rankNames.length * suitNames.length];
 		int i = 0;
 		for (int suit = 0; suit < suitNames.length; suit++) 
 			for (int rank = 0; rank < rankNames.length; rank++)
 				allCards[i++] = new Card(rank, suit);
+		
 		// create mapping from String representations to Card objects
 		for (Card card : allCards)
 			cardMap.put(card.toString(), card);
 	}
-        
+	
+	/**
+	 * Return an array of Strings with the single-character rank names.
+	 * @return an array of Strings with the single-character rank names
+	 */
+	public static String[] getRankNames() {
+		return rankNames.clone();
+	}
+	
+	/**
+	 * Return an array of Strings with the single-character suit names.
+	 * @return an array of Strings with the single-character suit names
+	 */
+	public static String[] getSuitNames() {
+		return suitNames.clone();
+	}
 
-	private int rank, suit;
+	/**
+	 * Given a Card String representation, return the associated Card.
+	 * @param cardString card String representation
+	 * @return Card associated with given Card String representation
+	 */
+	public static Card getCard(String cardString) {
+		return cardMap.get(cardString);
+	}
+
+	/**
+	 * Get the Card object associated with the given card identification integer.
+	 * @param cardId the unique integer identification number for the desired card
+	 * @return the Card object associated with the given card identification integer
+	 */
+	public static Card getCard(int cardId) {
+		return allCards[cardId];
+	}
+
+	/**
+	 * Get an array containing all cards.
+	 * @return an array containing all cards
+	 */
+	public static Card[] getAllCards() {
+		return allCards.clone();
+	}
+
+	// Non-static definitions
+	
+	private int rank; // the index of the rank String in rankNames
+	private int suit; // the index of the suit String in suitNames
 
 	/**
 	 * Create a card with the given rank and suit.
 	 * @param rank Card rank. Should be in range [0, NUM_RANKS - 1].
 	 * @param suit Card suit. Should be in range [0, NUM_SUITS - 1].
 	 */
-	public Card(int rank, int suit) {
+	private Card(int rank, int suit) {
 		this.rank = rank;
 		this.suit = suit;
 	}
@@ -55,7 +114,25 @@ public class Card {
 	public int getSuit() {
 		return suit;
 	}
-
+	
+	/**
+	 * Return whether or not the card is an ace.
+	 * @return whether or not the card is an ace
+	 */
+	public boolean isAce() {
+		return rank == 0;
+	}
+	
+	/**
+	 * Return unique integer card identification number according to "suit-major" ordering where cards are ordered
+	 * A, 2, ..., K within suits that are ordered alphabetically. This identification number is the 0-based suit 
+	 * integer times the number of ranks plus the 0-based rank number.
+	 * @return the 0-based suit integer times the number of ranks plus the 0-based rank number
+	 */
+	public int getCardId() {
+		return suit * NUM_RANKS + rank;
+	}
+	
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
@@ -63,7 +140,20 @@ public class Card {
 		return rankNames[rank] + suitNames[suit];
 	}
 
+	/**
+	 * Return whether or not this card equals another Card object.
+	 * @param other the other Card object being tested for equality.
+	 * @return whether or not this card equals another Card object
+	 */
 	public boolean equals(Card other) {
-		return this.rank == other.rank && this.suit == other.suit;		
+		return other != null && this.rank == other.rank && this.suit == other.suit;		
+	}
+	
+	/**
+	 * Print all card objects.
+	 * @param args (not used)
+	 */
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(allCards));
 	}
 }
--- PokerSquares/src/pokersquares/environment/PokerSquares.java
+++ PokerSquares/src/pokersquares/environment/PokerSquares.java
@@ -1,13 +1,11 @@
-package pokersquares.environment;
-
-import pokersquares.players.PokerSquaresPlayer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Stack;
-import java.io.*;
 
-/* The solitaire game "Poker Squares"
+/* A parameterized version of the solitaire game "Poker Squares" with variable point systems.
  * Author: Todd W. Neller
 
 Notes: 
@@ -15,8 +13,8 @@ Notes:
 A Poker Squares grid is represented as a 5-by-5 array of Card objects.  A null indicates an empty position.
 In the solitaire game of Poker Squares, a deck is initially shuffled.
 Each turn, the player draws a card from the deck and places it in any empty cell of a 5-by-5 grid.  Once placed, cards may not be moved.
-After the last cell is filled, each row and column are scored according to the American point system for Poker Squares hands:
-
+After the last cell is filled, each row and column are scored according to a point system for Poker Squares hands.
+For example, the American system is as follows:
 100 - Royal Flush: A T-J-Q-K-A sequence all of the same suit. Example: TC, JC, QC, KC, AC.
 75 - Straight Flush: Five cards in sequence all of the same suit. Example: AD, 2D, 3D, 4D, 5D.
 50 - Four of a Kind: Four cards of the same rank. Example: 9C, 9D, 9H, 9S, 6H.
@@ -36,35 +34,58 @@ For our purposes, a player is considered better if it has a higher expected game
 
 In our implementation, each turn a PokerSquaresPlayer will be passed (1) a Card object and (2) the number of milliseconds remaining in the game, 
 and will return a length 2 integer array with the row and column the player placed the card.  In the event that the player makes an illegal 
-play or "times out", i.e. runs out of time for play, the player loses with a final score of 0.
+play or "times out", i.e. runs out of time for play, the player loses with a final score of 10 times the minimum hand score.
 
-This file contains not only the code to run a simple demonstration game with a random player, but also has utility functions for scoring
-that will be useful for coding good players.
+This file contains not only the code to run a simple demonstration game with a random player, but also code to perform batch game testing,
+and tournament evaluation.
 
  */
 
 public class PokerSquares {
 	
 	public static final int SIZE = 5; // square grid size
-	public static final long GAME_MILLIS = 60000L; // 2013 contest maximum milliseconds per game
+	public static final long POINT_SYSTEM_MILLIS = 300000L; // EAAI-2016 contest maximum milliseconds for processing score table
+	public static final long GAME_MILLIS = 30000L; // EAAI-2016 contest maximum milliseconds per game
 
 	private PokerSquaresPlayer player; // current player
-	private long gameMillis; // maximum milliseconds for current game
+	private PokerSquaresPointSystem system; // current point system
+	private long gameMillis = GAME_MILLIS; // maximum milliseconds for current game
 	private boolean verbose = true; // whether or not to print move-by-move transcript of the game
 	private Card[][] grid = new Card[SIZE][SIZE]; // current game grid
 	private Random random = new Random(); // current game random number generator
+	private int minPoints; // minimum possible score for current point system.
 	
 	/**
+	 * Create a PokerSquares game with a given player and point system.
 	 * @param player Poker Squares player object
-	 * @param gameMillis maximum milliseconds permitted for game
+	 * @param system current Poker Squares point system
 	 */
-	public PokerSquares(PokerSquaresPlayer player, long gameMillis) {
+	public PokerSquares(PokerSquaresPlayer player, PokerSquaresPointSystem system) {
 		this.player = player;
-		this.gameMillis = gameMillis;
+		this.system = system;
+		minPoints = Integer.MAX_VALUE;
+		for (int points : system.getScoreTable())
+			if (points < minPoints)
+				minPoints = points;
+		minPoints *= 10;
+		final PokerSquaresPlayer PLAYER = player;
+		final PokerSquaresPointSystem SYSTEM = system;
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				PLAYER.setPointSystem(SYSTEM, POINT_SYSTEM_MILLIS);
+			}
+		});
+		thread.start();
+		try {
+			thread.join(POINT_SYSTEM_MILLIS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
-	 * Play a game of Poker Squares with the given PokerSquaresPlayer and time limit, returning the game score.
+	 * Play a game of Poker Squares and return the final game score.
 	 * @return final game score
 	 */
 	public int play() {
@@ -72,7 +93,7 @@ public class PokerSquares {
 
 		// shuffle deck
 		Stack<Card> deck = new Stack<Card>();
-		for (Card card : Card.allCards)
+		for (Card card : Card.getAllCards())
 			deck.push(card);
 		Collections.shuffle(deck, random);
 		
@@ -91,21 +112,83 @@ public class PokerSquares {
 			millisRemaining -= System.currentTimeMillis() - startTime;
 			if (millisRemaining < 0) { // times out
 				System.err.println("Player Out of Time");
-				return 0;
+				return minPoints;
 			}
 			if (play.length != 2 || play[0] < 0 || play[0] >= SIZE || play[1] < 0 || play[1] >= SIZE || grid[play[0]][play[1]] != null) { // illegal play
 				System.err.printf("Illegal play: %s\n", Arrays.toString(play));
-                                System.exit(0);
-				return 0;
+				return minPoints;
 			}
 			grid[play[0]][play[1]] = card;
 			cardsPlaced++;
 			if (verbose) {
-				printGrid(grid);
+				system.printGrid(grid);
 				System.out.println();
 			}
 		}
-		return getScore(grid);
+		return system.getScore(grid);
+	}
+	
+	/**
+	 * Given two-character card inputs from the given Scanner, play a game of Poker Squares and return the final
+	 * game score.
+	 * @param in Scanner object from which two-character card inputs will be read
+	 * @return final game score
+	 */
+	public int play(Scanner in) {
+		player.init();
+
+		// track remaining cards
+		ArrayList<Card> remaining = new ArrayList<Card>();
+		for (Card card : Card.getAllCards())
+			remaining.add(card);
+		Collections.shuffle(remaining, random);
+		
+		// clear grid
+		for (int row = 0; row < SIZE; row++)
+			for (int col = 0; col < SIZE; col++)
+				grid[row][col] = null;
+		
+		// play game
+		long millisRemaining = gameMillis;
+		int cardsPlaced = 0;
+		while (cardsPlaced < SIZE * SIZE) {
+			Card card = null;
+			while (card == null) {
+				System.out.println("Remaining cards: " + remaining);
+				System.out.print("Card? ");
+				String cardName = in.nextLine().trim().toUpperCase();
+				card = Card.getCard(cardName);
+				if (card == null) {
+					System.err.println("Error: Invalid card name");
+					continue;
+				}
+				if (!remaining.contains(card)) {
+					card = null;
+					System.err.println("Error: Card already played");
+					continue;
+				}
+				remaining.remove(card);
+			}
+			
+			long startTime = System.currentTimeMillis();
+			int[] play = player.getPlay(card, millisRemaining);
+			millisRemaining -= System.currentTimeMillis() - startTime;
+			if (millisRemaining < 0) { // times out
+				System.err.println("Player Out of Time");
+				return minPoints;
+			}
+			if (play.length != 2 || play[0] < 0 || play[0] >= SIZE || play[1] < 0 || play[1] >= SIZE || grid[play[0]][play[1]] != null) { // illegal play
+				System.err.printf("Illegal play: %s\n", Arrays.toString(play));
+				return minPoints;
+			}
+			grid[play[0]][play[1]] = card;
+			cardsPlaced++;
+			if (verbose) {
+				system.printGrid(grid);
+				System.out.println();
+			}
+		}
+		return system.getScore(grid);
 	}
 	
 	/**
@@ -115,19 +198,21 @@ public class PokerSquares {
 	 * @param verbose whether or not to provide verbose output of game play
 	 * @return integer array of game scores
 	 */
-	public double playSequence(int numGames, long startSeed, boolean verbose) {
+	public int[] playSequence(int numGames, long startSeed, boolean verbose) {
 		this.verbose = verbose;
+		if (verbose) {
+			System.out.printf("%d games starting at seed %d\nPoint system:\n%s\n", numGames, startSeed, system);
+		}
 		int[] scores = new int[numGames];
 		double scoreMean = 0;
-		int min = 10000, max = scores[0];
+		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 		for (int i = 0; i < numGames; i++) {
-			random.setSeed(startSeed + i);
+			setSeed(startSeed + i);
 			int score = play();
 			scores[i] = score;
 			scoreMean += score;
 			if (scores[i] < min) min = scores[i];
 			if (scores[i] > max) max = scores[i];
-                        //if(scores[i] < 36) System.out.println(startSeed + i);
 			System.out.println(score);
 		}
 		scoreMean /= numGames;
@@ -137,147 +222,104 @@ public class PokerSquares {
 			scoreStdDev += diff * diff;
 		}
 		scoreStdDev = Math.sqrt(scoreStdDev / numGames);
-                String Stats = "Mean Score: " +scoreMean+", Standard Deviation: "+scoreStdDev+", Minimum: "+min+", Maximum: "+max+", Games: "+numGames;
-		System.out.println(Stats);
-		return scoreMean;
+		System.out.printf("Score Mean: %f, Standard Deviation: %f, Minimum: %d, Maximum: %d\n", scoreMean, scoreStdDev, min, max);
+		return scores;
 	}
 	
 	/**
-	 * Print the current game grid and score.
-	 * @param grid current game grid 
+	 * Hold a Poker Squares tournament between the given players and point systems, the number of games played by 
+	 * each player with each point system, and the start seed determining the sequence of deals all will encounter, 
+	 * returning an array of doubles corresponding to the tournament score of each player.
+	 * Tournament scores are determined as follows. For each tournament, average scores are linearly scaled and 
+	 * transformed such that the player with the maximum or minimum average score receive a tournament score of 
+	 * 1.0 or 0.0 respectively, with all other players receiving linearly scaled tournament scores between 0.0 and 1.0.
+	 * For each scoring system, a player receives a tournament score, and in the ith position of the return array is
+	 * the sum of all tournament scores of the ith player.
+	 * @param players Poker Squares players taking part in the tournament
+	 * @param systems Poker Squares point systems used to evaluate players in the tournament
+	 * @param gamesPerSystem the number of games that will be played by each player with each point system
+	 * @param startSeed the start seed for the pseudorandom number generator that generates card deals
+	 * @return the sum of the tournament scores for each of the given players
 	 */
-        public static void printHand(Card[] hand,double score) {
-                for (int i = 0; i < SIZE; i++) {
-                    System.out.printf(" %s ", hand[i] == null ? "--" : hand[i].toString());
-                }
-                System.out.printf("%3f\n", score);
-        }
-        
-	public static void printGrid(Card[][] grid) {
-		// get scores
-		int[] handScores = getHandScores(grid);
-		int totalScore = 0;
-		for (int handScore : handScores)
-			totalScore += handScore;
-		
-		// print grid
-		for (int row = 0; row < SIZE; row++) {
-			for (int col = 0; col < SIZE; col++) 
-				System.out.printf(" %s ", grid[row][col] == null ? "--" : grid[row][col].toString());
-			System.out.printf("%3d\n", handScores[row]);
-		}
-		for (int col = 0; col < SIZE; col++) 
-			System.out.printf("%3d ", handScores[SIZE + col]);
-		System.out.printf("%3d Total\n", totalScore);
-	}
-	
-	/**
-	 * Get the score of the given Card grid.
-	 * @param grid Card grid
-	 * @return score of given Card grid
-	 */
-	public static int getScore(Card[][] grid) {
-		int[] handScores = getHandScores(grid);
-		int totalScore = 0;
-		for (int handScore : handScores)
-			totalScore += handScore;
-		return totalScore;
-	}
-	
-	/**
-	 * Get an int array with the individual hand scores of rows 0 through 4 followed by columns 0 through 4. 
-	 * @param grid 2D Card array representing play grid
-	 * @return an int array with the individual hand scores of rows 0 through 4 followed by columns 0 through 4. 
-	 */
-	public static int[] getHandScores(Card[][] grid) {
-		int[] handScores = new int[2 * SIZE];
-		for (int row = 0; row < SIZE; row++) {
-			Card[] hand = new Card[SIZE];
-			for (int col = 0; col < SIZE; col++)
-				hand[col] = grid[row][col];
-			handScores[row] = getHandScore(hand);
-		}
-		for (int col = 0; col < SIZE; col++) {
-			Card[] hand = new Card[SIZE];
-			for (int row = 0; row < SIZE; row++)
-				hand[row] = grid[row][col];
-			handScores[SIZE + col] = getHandScore(hand);
-		}
-		return handScores;
-	}
-	
-	/**
-	 * Get the score of the given Card hand.
-	 * @param hand Card hand
-	 * @return score of given Card hand.
-	 */
-	public static int getHandScore(Card[] hand) {
-		// Compute counts
-		int[] rankCounts = new int[Card.NUM_RANKS];
-		int[] suitCounts = new int[Card.NUM_SUITS];
-		for (Card card : hand)
-			if (card != null) {
-				rankCounts[card.getRank()]++;
-				suitCounts[card.getSuit()]++;
+	public static double[] playTournament(
+			ArrayList<PokerSquaresPlayer> players,
+			ArrayList<PokerSquaresPointSystem> systems,
+			int gamesPerSystem, long startSeed) {
+		double[] tournamentScores = new double[players.size()];
+		for (PokerSquaresPointSystem system : systems) { // for each point system
+			System.out.println("Point System:\n" + system);
+			int[] totalScores = new int[players.size()];
+			for (int i = 0; i < players.size(); i++) { // for each player
+				PokerSquaresPlayer player = players.get(i);
+				System.out.printf("Player: \"%s\"\n", player.getName());
+				int[] scores = new PokerSquares(player, system).playSequence(gamesPerSystem, startSeed, false);
+				for (int score : scores)
+					totalScores[i] += score;
+				System.out.printf("Player \"%s\" total score: %d\n", player.getName(), totalScores[i]);
 			}
-		
-		// Compute count of rank counts
-		int maxOfAKind = 0;
-		int[] rankCountCounts = new int[hand.length + 1];
-		for (int count : rankCounts) {
-				rankCountCounts[count]++;
-				if (count > maxOfAKind)
-					maxOfAKind = count;
+			int maxTotal = Integer.MIN_VALUE;
+			int minTotal = Integer.MAX_VALUE;
+			for (int totalScore : totalScores) {
+				if (totalScore > maxTotal)
+					maxTotal = totalScore;
+				if (totalScore < minTotal)
+					minTotal = totalScore;				
 			}
-			
-		// Flush check
-		boolean hasFlush = false;
-		for (int i = 0; i < Card.NUM_SUITS; i++)
-			if (suitCounts[i] != 0) {
-				if (suitCounts[i] == hand.length)
-					hasFlush = true;
-				break;
+			for (int i = 0; i < players.size(); i++) { // for each player
+				double normalizedTotal = (double) (totalScores[i] - minTotal) / (maxTotal - minTotal);
+				System.out.println("Player \"" + players.get(i).getName() + "\" normalized score: " + normalizedTotal);
+				tournamentScores[i] += normalizedTotal;
 			}
-		
-		// Straight check
-		boolean hasStraight = false;
-		boolean hasRoyal = false;
-		int rank = 0;
-		while (rank <= Card.NUM_RANKS - 5 && rankCounts[rank] == 0)
-			rank++;
-		hasStraight = (rank <= Card.NUM_RANKS - 5 && rankCounts[rank] == 1 && rankCounts[rank + 1] == 1 && rankCounts[rank + 2] == 1 && rankCounts[rank + 3] == 1 && rankCounts[rank + 4] == 1);
-		if (rankCounts[0] == 1 && rankCounts[12] == 1 && rankCounts[11] == 1 && rankCounts[10] == 1 && rankCounts[9] == 1) 
-			hasStraight = hasRoyal = true;
-		
-		// Return score
-		if (hasFlush) {
-			if (hasRoyal)
-				return 100; // Royal Flush
-			if (hasStraight)
-				return 75; // Straight Flush
 		}
-		if (maxOfAKind == 4)
-			return 50; // Four of a Kind
-		if (rankCountCounts[3] == 1 && rankCountCounts[2] == 1)
-			return 25; // Full House
-		if (hasFlush)
-			return 20; // Flush
-		if (hasStraight)
-			return 15; // Straight
-		if (maxOfAKind == 3)
-			return 10; // Three of a Kind
-		if (rankCountCounts[2] == 2)
-			return 5; // Two Pair
-		if (rankCountCounts[2] == 1)
-			return 2; // One Pair
-		return 0; // Otherwise, score nothing.
+		double max = Double.NEGATIVE_INFINITY;
+		for (double score : tournamentScores) {
+			if (score > max)
+				max = score;
+		}
+		System.out.printf("%20s %s\n", "Player", "Tournament Score");
+		for (int i = 0; i < players.size(); i++) { // for each player
+			System.out.printf("%20s %f\n", players.get(i).getName(), tournamentScores[i]);
+		}
+		
+		return tournamentScores;
 	}
 	
 	/**
 	 * Set the seed of the game pseudorandom number generator.
 	 * @param seed pseudorandom number generator seed
 	 */
-	public void setSeed(long seed) {
+	private void setSeed(long seed) {
 		random.setSeed(seed);
+	}
+	
+	
+	/**
+	 * Demonstrate single/batch game play testing and tournament evaluation of PokerSquaresPlayers.
+	 * @param args (not used)
+	 */
+	public static void main(String[] args) {
+		// Demonstration of single game play (30 seconds)
+		System.out.println("Single game demo:");
+		PokerSquaresPointSystem.setSeed(0L);
+		PokerSquaresPointSystem system = PokerSquaresPointSystem.getAmeritishPointSystem();
+		System.out.println(system);
+		new PokerSquares(new GreedyMCPlayer(2), PokerSquaresPointSystem.getAmeritishPointSystem()).play();
+
+		// Demonstration of batch game play (30 seconds per game)
+		System.out.println("\n\nBatch game demo:");
+		System.out.println(system);
+		new PokerSquares(new GreedyMCPlayer(2), PokerSquaresPointSystem.getAmeritishPointSystem()).playSequence(3, 0, false);
+		
+		// Demonstration of tournament evaluation (3 players, 2 point systems, 100 x 30s games for each of the 3*2=6 player-system pairs) 
+		System.out.println("\n\nTournament evaluation demo:");
+		ArrayList<PokerSquaresPlayer> players = new ArrayList<PokerSquaresPlayer>();
+		players.add(new RandomPlayer());
+		players.add(new GreedyMCPlayer(0));
+		players.add(new GreedyMCPlayer(2));
+		ArrayList<PokerSquaresPointSystem> systems = new ArrayList<PokerSquaresPointSystem>();
+		PokerSquaresPointSystem.setSeed(0L);
+		systems.add(PokerSquaresPointSystem.getAmeritishPointSystem());
+		systems.add(PokerSquaresPointSystem.getRandomPointSystem());
+		PokerSquares.playTournament(players, systems, 100, 0L); // use fewer games per system for faster testing
 	}
 }
